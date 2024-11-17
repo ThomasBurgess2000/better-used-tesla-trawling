86f3b009565d6d51ed0a80ce9a1edb3fbf7c6b47
Thomas Burgess <thomasburgess2000@gmail.com>
2024-11-17 14:15:52 -0600

    Update rules and github link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://github.com/ThomasBurgess2000"
+          href="https://github.com/ThomasBurgess2000/better-used-tesla-trawling"
           target="_blank"
           rel="noopener noreferrer"
         >
